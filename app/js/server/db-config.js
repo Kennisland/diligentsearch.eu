d0f23d22494c78595673e58cf88badf2de46f04f
arnaudB <arnaud.brun.vidal@gmail.com>
2017-01-16 10:15:52 +0100

    add db config default value to change for further exportations

--- app/js/server/db-config.js
+++ app/js/server/db-config.js
@@ -1,12 +1,9 @@
 exports.dbConfig = function(){
 	return {
-		host     : 'localhost',
-		user     : 'phpmyadmin',
-		password : 'diligentsearch',
-		database : 'diligent_search'		
+		host     : '<host_name>',
+		user     : '<mysql_username>',
+		password : '<mysql_passwor_for_mysql_user>',
+		database : '<mysql_database_name>'	
 	};
-	// host     : '<host_name>',
-	// user     : '<mysql_username>',
-	// password : '<mysql_passwor_for_mysql_user>',
-	// database : '<mysql_database_name>'
+
 }
