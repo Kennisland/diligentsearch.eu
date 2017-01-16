e9e32ea149a55866f5d239f4eb52ed1ce4d5c5b6
arnaudB <arnaud.brun.vidal@gmail.com>
2017-01-16 10:18:35 +0100

    remove db-config file from git index

--- app/js/server/db-config.js
+++
@@ -1,9 +0,0 @@
-exports.dbConfig = function(){
-	return {
-		host     : '<host_name>',
-		user     : '<mysql_username>',
-		password : '<mysql_passwor_for_mysql_user>',
-		database : '<mysql_database_name>'	
-	};
-
-}
