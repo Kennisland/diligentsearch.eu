a99a72a818d28a04aa164b1472d65ffeeeb5d88d
arnaudB <arnaud.brun.vidal@gmail.com>
2017-02-09 11:51:25 +0100

    update of database schema : triggers inside / no data

--- diligentsearch-shema.sql
+++ diligentsearch-shema.sql
@@ -1,203 +1,318 @@
--- MySQL dump 10.13  Distrib 5.7.17, for Linux (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb1+deb.cihar.com~xenial.2
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: diligent_search
--- ------------------------------------------------------
--- Server version	5.7.17-0ubuntu0.16.04.1
+-- Host: localhost
+-- Generation Time: Feb 09, 2017 at 11:48 AM
+-- Server version: 5.7.17-0ubuntu0.16.04.1
+-- PHP Version: 7.0.13-0ubuntu0.16.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `diligent_search`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `Block`
 --
 
-DROP TABLE IF EXISTS `Block`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Block` (
-  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `id` int(4) NOT NULL,
   `workId` int(3) NOT NULL,
-  `json` text NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `blockWorkId` (`workId`),
-  CONSTRAINT `blockWorkId` FOREIGN KEY (`workId`) REFERENCES `Work` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `json` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `Country`
 --
 
-DROP TABLE IF EXISTS `Country`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Country` (
-  `id` int(2) NOT NULL AUTO_INCREMENT,
+  `id` int(2) NOT NULL,
   `code` varchar(3) NOT NULL,
-  `name` varchar(30) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `DecisionTree`
 --
 
-DROP TABLE IF EXISTS `DecisionTree`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `DecisionTree` (
-  `id` int(3) NOT NULL AUTO_INCREMENT,
+  `id` int(3) NOT NULL,
   `workId` int(3) NOT NULL,
-  `json` text NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `DecisionTreeWorkConstraint` (`workId`),
-  CONSTRAINT `DecisionTreeWorkConstraint` FOREIGN KEY (`workId`) REFERENCES `Work` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `json` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `Form`
 --
 
-DROP TABLE IF EXISTS `Form`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Form` (
-  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `id` int(5) NOT NULL,
   `workId` int(3) NOT NULL,
   `hook` text NOT NULL,
   `version` int(3) NOT NULL DEFAULT '0',
-  `json` text NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `formWorkConstraint` (`workId`),
-  CONSTRAINT `formWorkConstraint` FOREIGN KEY (`workId`) REFERENCES `Work` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8_unicode_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`phpmyadmin`@`localhost`*/ /*!50003 TRIGGER `FormVersioning` BEFORE INSERT ON `Form` FOR EACH ROW BEGIN
-	DECLARE c INT;
+  `json` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Triggers `Form`
+--
+DELIMITER $$
+CREATE TRIGGER `FormVersioning` BEFORE INSERT ON `Form` FOR EACH ROW BEGIN
+  DECLARE c INT;
     SET c = (SELECT COUNT(*) FROM Form WHERE hook = NEW.hook);
     IF c=0 THEN
         SET NEW.version = 1;
     ELSE
-    	SET NEW.version = (SELECT (MAX(version)+1) FROM Form WHERE hook = NEW.hook);
-	END IF;
-END */;;
+      SET NEW.version = (SELECT (MAX(version)+1) FROM Form WHERE hook = NEW.hook);
+  END IF;
+END
+$$
 DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `Question`
 --
 
-DROP TABLE IF EXISTS `Question`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Question` (
-  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `id` int(4) NOT NULL,
   `workId` int(3) NOT NULL,
-  `json` text NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `QuestionWorkId` (`workId`),
-  CONSTRAINT `QuestionWorkId` FOREIGN KEY (`workId`) REFERENCES `Work` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `json` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `Result`
 --
 
-DROP TABLE IF EXISTS `Result`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Result` (
-  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `id` int(4) NOT NULL,
   `workId` int(3) NOT NULL,
-  `json` text NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `ResultWorkId` (`workId`),
-  CONSTRAINT `ResultWorkId` FOREIGN KEY (`workId`) REFERENCES `Work` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `json` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `SharedRefValue`
 --
 
-DROP TABLE IF EXISTS `SharedRefValue`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `SharedRefValue` (
-  `id` int(3) NOT NULL AUTO_INCREMENT,
+  `id` int(3) NOT NULL,
   `countryId` int(2) NOT NULL,
-  `json` text NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `RefValuesCountry` (`countryId`),
-  CONSTRAINT `RefValuesCountry` FOREIGN KEY (`countryId`) REFERENCES `Country` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `json` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `SharedUserInput`
 --
 
-DROP TABLE IF EXISTS `SharedUserInput`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `SharedUserInput` (
-  `id` int(3) NOT NULL AUTO_INCREMENT,
+  `id` int(3) NOT NULL,
   `countryId` int(2) NOT NULL,
-  `json` text NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `UserInputsCountry` (`countryId`),
-  CONSTRAINT `UserInputsCountry` FOREIGN KEY (`countryId`) REFERENCES `Country` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `json` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `Work`
 --
 
-DROP TABLE IF EXISTS `Work`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Work` (
-  `id` int(3) NOT NULL AUTO_INCREMENT,
+  `id` int(3) NOT NULL,
   `countryId` int(2) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `countryId` (`countryId`) USING BTREE,
-  CONSTRAINT `WorkCountry#` FOREIGN KEY (`countryId`) REFERENCES `Country` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Block`
+--
+ALTER TABLE `Block`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `blockWorkId` (`workId`);
+
+--
+-- Indexes for table `Country`
+--
+ALTER TABLE `Country`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `DecisionTree`
+--
+ALTER TABLE `DecisionTree`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_work_id_decisionTree` (`workId`);
+
+--
+-- Indexes for table `Form`
+--
+ALTER TABLE `Form`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `formWorkConstraint` (`workId`);
+
+--
+-- Indexes for table `Question`
+--
+ALTER TABLE `Question`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `QuestionWorkId` (`workId`);
+
+--
+-- Indexes for table `Result`
+--
+ALTER TABLE `Result`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `ResultWorkId` (`workId`);
+
+--
+-- Indexes for table `SharedRefValue`
+--
+ALTER TABLE `SharedRefValue`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `RefValuesCountry` (`countryId`);
+
+--
+-- Indexes for table `SharedUserInput`
+--
+ALTER TABLE `SharedUserInput`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `UserInputsCountry` (`countryId`);
+
+--
+-- Indexes for table `Work`
+--
+ALTER TABLE `Work`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `countryId` (`countryId`) USING BTREE;
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Block`
+--
+ALTER TABLE `Block`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `Country`
+--
+ALTER TABLE `Country`
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `DecisionTree`
+--
+ALTER TABLE `DecisionTree`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `Form`
+--
+ALTER TABLE `Form`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `Question`
+--
+ALTER TABLE `Question`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `Result`
+--
+ALTER TABLE `Result`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `SharedRefValue`
+--
+ALTER TABLE `SharedRefValue`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `SharedUserInput`
+--
+ALTER TABLE `SharedUserInput`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `Work`
+--
+ALTER TABLE `Work`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Block`
+--
+ALTER TABLE `Block`
+  ADD CONSTRAINT `fk_work_id` FOREIGN KEY (`workId`) REFERENCES `Work` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `DecisionTree`
+--
+ALTER TABLE `DecisionTree`
+  ADD CONSTRAINT `fk_work_id_decisionTree` FOREIGN KEY (`workId`) REFERENCES `Work` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `Form`
+--
+ALTER TABLE `Form`
+  ADD CONSTRAINT `formWorkConstraint` FOREIGN KEY (`workId`) REFERENCES `Work` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `Question`
+--
+ALTER TABLE `Question`
+  ADD CONSTRAINT `QuestionWorkId` FOREIGN KEY (`workId`) REFERENCES `Work` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `Result`
+--
+ALTER TABLE `Result`
+  ADD CONSTRAINT `ResultWorkId` FOREIGN KEY (`workId`) REFERENCES `Work` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `SharedRefValue`
+--
+ALTER TABLE `SharedRefValue`
+  ADD CONSTRAINT `RefValuesCountry` FOREIGN KEY (`countryId`) REFERENCES `Country` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `SharedUserInput`
+--
+ALTER TABLE `SharedUserInput`
+  ADD CONSTRAINT `UserInputsCountry` FOREIGN KEY (`countryId`) REFERENCES `Country` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `Work`
+--
+ALTER TABLE `Work`
+  ADD CONSTRAINT `WorkCountry#` FOREIGN KEY (`countryId`) REFERENCES `Country` (`id`) ON DELETE CASCADE;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2017-01-31 14:00:13
