59f4acfb26494885e2b6a0a5338412526bf11917
arnaudB <arnaud.brun.vidal@gmail.com>
2017-02-23 16:19:36 +0100

    modal answer/question selection part fullsized

--- app/template-controller/data-modal/block-modal.js
+++ app/template-controller/data-modal/block-modal.js
@@ -21,25 +21,24 @@ html_block = `
 				</div>
 
 				<div id="block-questions" class="form-group">
-					<label>Questions contained within this block : </label>
+					<label style="float-left">Questions contained within this block : </label>
+					<div id="block-questions-management" style="float:right" >				
+						<button id="addQuestion" type="button">+</button>
+						<button id="delLastQuestion" type="button">-</button>
+					</div>
+					
 
-					<div style="overflow:auto;">
-						<div style="float:left; width:70%; margin-left:3%">
-							<table class"table table-responsive table-bordered table-stripped" style="width:100%">
-								<thead>
-									<th style="width:10%;	min-width:10%;	max-width:10%; text-align:center">#</th>
-									<th style="width:25%;	min-width:25%;	max-width:25%; text-align:center">Type</th>
-									<th style="width:60%;	min-width:60%;	max-width:60%; text-align:center">Question</th>
-									<th style="width:5%;	min-width:5%;	max-width:5%;  text-align:center"></th>
-								</thead>
-								<tbody id="block-questions-selection">
-								</tbody>
-							</table>
-						</div>
-						<div id="block-questions-management" style="float:right" >				
-							<button id="addQuestion" type="button">+</button>
-							<button id="delLastQuestion" type="button">-</button>
-						</div>
+					<div style="overflow:auto; width:100%; margin-left:3%">
+						<table class"table table-responsive table-bordered table-stripped" style="width:100%">
+							<thead>
+								<th style="width:10%;	min-width:10%;	max-width:10%; text-align:center">#</th>
+								<th style="width:25%;	min-width:25%;	max-width:25%; text-align:center">Type</th>
+								<th style="width:60%;	min-width:60%;	max-width:60%; text-align:center">Question</th>
+								<th style="width:5%;	min-width:5%;	max-width:5%;  text-align:center"></th>
+							</thead>
+							<tbody id="block-questions-selection">
+							</tbody>
+						</table>
 					</div>
 				</div>
 
@@ -74,14 +73,12 @@ function loadBlock(index, blockElt){
 	$('#block-introduction').val(blockElt.introduction);
 
 	for (var i = 0; i < blockElt.questions.length; i++) {
-
-		console.log("Block : adding question");
-
 		// There is always a 'default' question, so avoid to add one on the first iteration
 		if(i != 0){
 			addQuestion();			
 		}
 		var idx = blockElt.questions[i];
+
 		for (var j = 0; j < questions.length; j++) {
 			if(idx == questions[j].id){
 				var q = questions[j];
@@ -92,7 +89,6 @@ function loadBlock(index, blockElt){
 			}
 		}
 	}
-
 	$('#add-blockModal').modal('show');
 }
 
