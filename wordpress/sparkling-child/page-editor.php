1eff6954319d87fa6876a9209aee6f2de9b1eca1
Maarten Zeinstra <mz@kl.nl>
2017-04-11 15:54:08 +0200

    Reworked password protected functionality of Wordpress

--- wordpress/sparkling-child/page-editor.php
+++ wordpress/sparkling-child/page-editor.php
@@ -11,26 +11,36 @@
 
 get_header('custom'); ?>
 
-	<?php if ( post_password_required() ) : ?>
-		<?php get_the_password_form(); ?>
-	<?php else : ?>
-		<div id="primary" class="content-area">
+<?php if ( post_password_required() ) : ?>
+	<div id="primary" class="content-area">
 
-			<main id="main" class="entry-content row" role="main">
+		<main id="main" class="entry-content row" role="main">
 			
-					<div class="col-lg-4 col-md-4 col-xs-12 col-sm-12" >
-					<!-- style="min-height:100%" -->
-						<div id="data-editor" class="left-panel"></div>					
-					</div>
-					<div class="col-lg-8 col-md-8 col-xs-12 col-sm-12" >
-					<!-- style="min-height:100%" -->
-						<div id="graph-editor" class="right-panel"></div>
-					</div>
+				<?php echo get_the_password_form(); ?>
+			
+		</main><!-- #main -->
+	</div><!-- #primary -->
+<?php else : ?>
+	<div id="primary" class="content-area">
+
+		<main id="main" class="entry-content row" role="main">
+			
+				<div class="col-lg-4 col-md-4 col-xs-12 col-sm-12" >
+				<!-- style="min-height:100%" -->
+					<div id="data-editor" class="left-panel"></div>					
+				</div>
+				<div class="col-lg-8 col-md-8 col-xs-12 col-sm-12" >
+				<!-- style="min-height:100%" -->
+					<div id="graph-editor" class="right-panel"></div>
+				</div>
 			
 
-				<div id="modal-section"></div>
-			</main><!-- #main -->
-		</div><!-- #primary -->
-	<?php endif; ?>
+			<div id="modal-section"></div>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+<?php endif; ?>
+
+
+	
 
 <?php get_footer('editor'); ?>
